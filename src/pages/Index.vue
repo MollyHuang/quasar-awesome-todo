d0b1401836410086794da834f29dd58a28f37acc
Molly Huang <molly.h@gmail.com>
2021-03-30 21:26:44 +0800

    #12. Two-way Data Binding with v-model

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,16 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <input v-model="message">
+    <h5>{{ message }}</h5>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data() {
+    return {
+      message: "I love Vue.js so hard"
+    }
+  }
 }
 </script>
