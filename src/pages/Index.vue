3374791b1bb400a827ba008bfdd9e6f266cb813f
Molly Huang <molly.h@gmail.com>
2021-03-31 19:25:50 +0800

    #26-v-for-Displaying-more-details

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,11 @@
   <q-page padding>
 
     <ul>
-      <li v-for="task in tasks">{{task}}</li>
+      <li v-for="(task, index) in tasks">
+        <div>{{ task.name }} {{ index }}</div>
+        <small>{{ task.dueDate }} {{ task.dueTime }}</small>
+        <button @click="deleteTask(index)">X</button>
+      </li>
     </ul>
 
   </q-page>
@@ -12,7 +16,28 @@
 export default {
   data() {
     return {
-      tasks: ['Go to shop', 'Get bananas', 'Get apples']
+      tasks: [
+        {
+          name: 'Go to shop',
+          dueDate: '2019/05/12',
+          dueTime: '18:30'
+        },
+        {
+          name:'Get bananas',
+          dueDate: '2019/05/13',
+          dueTime: '14:00'
+        },
+        {
+          name:'Get apples',
+          dueDate: '2019/05/14',
+          dueTime: '16:00'
+        }, 
+      ]
+    }
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
     }
   }
 }
