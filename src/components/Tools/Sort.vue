006273aeba3f74cf6a52b0fa01f4ef01a43f5dc2
Molly Huang <molly.h@gmail.com>
2021-04-05 21:17:24 +0800

    #95. Add a Sort Dropdown Component to the Page

---
+++ src/components/Tools/Sort.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-select
+    filled
+    v-model="model"
+    :options="options"
+    label="Sort by"
+    class="col q-ml-sm"
+    stack-label />
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        model: null,
+
+        options: [
+          'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+        ]
+      }
+    }
+  }
+</script>
+
+<style scope>
+  .q-select {
+    flex: 0 0 112px;
+  }
+</style>
--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -3,6 +3,7 @@
 
     <div class="row q-mb-lg">
       <search />
+      <sort />
     </div>
 
     <p v-if="search && !Object.keys(tasksTodo).length && !Object.keys(tasksCompleted).length">
@@ -65,7 +66,8 @@ export default {
     "task-completed": require("components/Tasks/TaskCompleted.vue").default,
     "no-tasks": require("components/Tasks/NoTasks.vue").default,
     "add-task": require("components/Tasks/Modals/AddTask.vue").default,
-    "search": require("components/Tools/Search.vue").default
+    "search": require("components/Tools/Search.vue").default,
+    "sort": require("components/Tools/Sort.vue").default
   }
 };
 </script>
