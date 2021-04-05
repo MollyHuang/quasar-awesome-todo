53e614d2da6c5ad7323c8df638859cc352ca6d8b
Molly Huang <molly.h@gmail.com>
2021-04-05 19:24:45 +0800

    #87. Use a Computed Property Setter to set the Vuex search value

--- src/components/Tools/Search.vue
+++ src/components/Tools/Search.vue
@@ -23,8 +23,13 @@
   export default {
     computed: {
       ...mapState('tasks', ['search']),
-      searchField() {
-        return this.search
+      searchField: {
+        get() {
+          return this.search
+        },
+        set(value) {
+          console.log("value:", value)
+        }
       }
     }
   }
