9b40773de4c4276f8f13932453ed74fbc24079fa
Molly Huang <molly.h@gmail.com>
2021-04-07 23:17:26 +0800

    112. Add a "Show 12 hour time format" Setting

--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -1,15 +1,28 @@
 <template>
   <q-page padding>
-    <p>Settings Page</p>
+    <q-list bordered padding>
+      <q-item-label header>Settings</q-item-label>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Show 12 hour time format</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-toggle 
+            v-model="show12hourTimeformat"
+            color="blue"></q-toggle>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      show12hourTimeformat: true
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
