f3999a49c3bc569adda1f0ec8842e82236ce1f6b
Molly Huang <molly.h@gmail.com>
2021-04-11 01:06:20 +0800

    147. fix router-auth.js

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -2,10 +2,8 @@ import { LocalStorage } from 'quasar'
 
 export default ({ router }) => {
   router.beforeEach((to, from, next) => {
-    let loggedIn = LocalStorage.getIndex("loggedIn")
-    // console.log("to:", to)
-    // console.log("from:", from)
-    if (!loggedIn && to.path != "/auth") {
+    let loggedIn = LocalStorage.getItem("loggedIn")
+    if (!loggedIn && to.path !== "/auth") {
       next("/auth")
     }
     else {
