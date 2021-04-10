1d8e4e40560eda1e48fd79c8dc3d7aa1f8141930
Molly Huang <molly.h@gmail.com>
2021-04-10 12:56:27 +0800

    126. Register - Create Component & Add a Banner

---
+++ src/components/Auth/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-banner class="bg-grey-3">
+      <template v-slot:avatar>
+        <q-icon name="account_circle" color="primary" />
+      </template>
+      Register to access your todo anywhere!
+    </q-banner>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
