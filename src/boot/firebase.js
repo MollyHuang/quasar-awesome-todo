f9f41c87125e06b5e7306f0cb1f78277c06d6b30
Molly Huang <molly.h@gmail.com>
2021-04-10 21:27:53 +0800

    135. Create a Firebase Project 136. Setup Authentication 137. Module Introduction 138. Add Firebase to the Project using Boot File

---
+++ src/boot/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+
+import "firebase/auth"
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD43-9nIeCVQlS6Z_FO8mSh6Cjvxlh7IPo",
+  authDomain: "awesome-todo-baf5f.firebaseapp.com",
+  databaseURL: "https://awesome-todo-baf5f-default-rtdb.firebaseio.com",
+  projectId: "awesome-todo-baf5f",
+  storageBucket: "awesome-todo-baf5f.appspot.com",
+  messagingSenderId: "292208937831",
+  appId: "1:292208937831:web:34c430c9fcd857dddc3dc2"
+}
+let firebaseApp = firebase.initializeApp(firebaseConfig)
+let firebaseAuth = firebaseApp.auth
+
+export { firebaseAuth }
