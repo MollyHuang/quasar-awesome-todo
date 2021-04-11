8651eeacc83f39ece676e215d9413a0640a65dec
Molly Huang <molly.h@gmail.com>
2021-04-11 10:49:34 +0800

    156. Connect to the Database

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app"
 
 import "firebase/auth"
+import "firebase/database"
 
 var firebaseConfig = {
   apiKey: "AIzaSyD43-9nIeCVQlS6Z_FO8mSh6Cjvxlh7IPo",
@@ -13,5 +14,6 @@ var firebaseConfig = {
 }
 let firebaseApp = firebase.initializeApp(firebaseConfig)
 let firebaseAuth = firebaseApp.auth()
+let firebaseDb = firebaseApp.database()
 
-export { firebaseAuth }
+export { firebaseAuth, firebaseDb }
