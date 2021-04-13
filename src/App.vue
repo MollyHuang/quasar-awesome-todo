cac7af1074aeaecc7352becab3b8a2418656e009
Molly Huang <molly.h@gmail.com>
2021-04-13 22:29:54 +0800

    195. Menu - Add a Settings Option 196. Menu - Listen to Settings Option with IPC Receive / Platform Detection

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,12 @@
       ...mapActions('auth', ['handleAuthStateChange']),
     },
     mounted() {
+      if (this.$q.platform.is.electron) {
+        require('electron').ipcRenderer.on('show-settings', () => {
+          this.$router.push('/settings')
+        })
+      }
+
       this.getSettings()
       this.handleAuthStateChange()
     }
