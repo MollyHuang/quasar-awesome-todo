6f1d003c98ef27bbf4a5d475acca7759a03684bd
Molly Huang <molly.h@gmail.com>
2021-04-18 17:49:14 +0800

    199. Listen for the Quit Event and Quit the App

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, nativeTheme, Menu } from 'electron'
+import { app, BrowserWindow, nativeTheme, Menu, ipcMain } from 'electron'
 import { menuTemplate } from './electron-main-menu-template'
 
 try {
@@ -60,5 +60,13 @@ app.on('ready', () => {
  */
 
 app.on('window-all-closed', () => {
+  app.quit()
+})
+
+/**
+ * ipc events
+ */
+
+ipcMain.on('quit-app', () => {
   app.quit()
 })
